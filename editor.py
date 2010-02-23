f900408f352d6039b1d60fcb06caea8aa995ef3d
Lee Harr <missive@hotmail.com>
2010-02-23 17:17:18 -0500

    fix license statements. add copy of GPLv3

--- editor.py
+++ editor.py
@@ -1,21 +1,19 @@
-#!/usr/bin/env python
-
-# Copyright 2009 Lee Harr
+# Copyright 2010 Lee Harr
 #
-# This file is part of pybotwar.
+# This file is part of pynguin.
 #
-# Pybotwar is free software: you can redistribute it and/or modify
+# Pynguin is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Pybotwar is distributed in the hope that it will be useful,
+# Pynguin is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Pybotwar.  If not, see <http://www.gnu.org/licenses/>.
+# along with Pynguin.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import os
