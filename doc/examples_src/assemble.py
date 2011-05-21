ee154813c0193c0af810d2e7dbe4ff2745b2885b
Lee Harr <missive@hotmail.com>
2011-05-21 18:23:41 -0400

    write new format

--- doc/examples_src/assemble.py
+++ doc/examples_src/assemble.py
@@ -1,25 +1,59 @@
 import os
+import zipfile
+import time
+
+def zip_pyn(ex, zfp, files, d):
+    VERSION = 'pyn01'
+
+    z = zipfile.ZipFile(zfp, 'w')
+    for f in files:
+        exp = os.path.join(ex, f)
+        if '@@history@@' in f:
+            data = open(exp).read()
+            history = data.decode('utf-8')
+
+            zipi = zipfile.ZipInfo()
+            zipi.filename = os.path.join(d, f)
+            zipi.compress_type = zipfile.ZIP_DEFLATED
+            zipi.date_time = time.localtime()
+            zipi.external_attr = 0644 << 16
+            zipi.comment = VERSION
+            z.writestr(zipi, history.encode('utf-8'))
+
+        elif f.endswith('.py'):
+            data = open(exp).read()
+            code = data.decode('utf-8')
+
+            zipi = zipfile.ZipInfo()
+            zipi.filename = os.path.join(d, f)
+            zipi.compress_type = zipfile.ZIP_DEFLATED
+            zipi.date_time = time.localtime()
+            zipi.external_attr = 0644 << 16
+            zipi.comment = VERSION
+            z.writestr(zipi, code.encode('utf-8'))
+
+        else:
+            print 'ERROR. Unknown file: %s' % f
+
 
 def make_pyn(ex):
-    os.chdir(ex)
-    history = open('@@history@@', 'w')
+    histf = os.path.join(ex, '@@history@@')
+    history = open(histf, 'w')
     history.close()
 
     if ex.endswith('_pynd'):
         exfile = '%s.pyn' % ex[:-5]
+        d = ex
     else:
         exfile = '%s.pyn' % ex
+        d = ex + '_pynd'
 
-    if os.path.exists(exfile):
-        os.remove(exfile)
-    cmd = 'zip -r %s *' % exfile
-    os.system(cmd)
+    fp = os.path.join('..', 'examples', exfile)
+    files = os.listdir(ex)
 
-    os.chdir('..')
-
-    fromfile = '%s/%s' % (ex, exfile)
-    tofile = '../examples/%s' % exfile
-    os.rename(fromfile, tofile)
+    print fp
+    print files
+    zip_pyn(ex, fp, files, d)
 
 def make_all():
     dirs = get_ex_dirs()
@@ -50,3 +84,5 @@ if __name__ == '__main__':
         make_all()
     elif '-C' in sys.argv:
         cleanup()
+    elif len(sys.argv)==2:
+        make_pyn(sys.argv[1])
