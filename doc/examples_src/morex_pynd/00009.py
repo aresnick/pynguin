fc8e822e3f7a289c89bb817df136f5de99e817df
Lee Harr <missive@hotmail.com>
2011-05-29 14:43:48 -0400

    add example morex/arc

---
+++ doc/examples_src/morex_pynd/00009.py
@@ -0,0 +1,16 @@
+def arc(w=150, b=50, e=500, rstep=10):
+    from math import sqrt
+    pen = pynguin.pen
+    penup()
+
+    fd(b)
+
+    h = b
+    while h < e:
+        r = sqrt(w**2 + h**2)
+        circle(r, True)
+        fd(rstep)
+        h += rstep
+
+    if pen:
+        pendown()
