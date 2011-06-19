738deb7179af552072ea47af95f9b782671d1189
Lee Harr <missive@hotmail.com>
2011-06-19 18:02:13 -0400

    add comment referencing multi.pyn

--- doc/examples_src/threaded_pynd/00000.py
+++ doc/examples_src/threaded_pynd/00000.py
@@ -10,6 +10,12 @@ def make(n):
     threaded commands with, you can
     call make(0) in order to set up
     the thread kill mechanism.
+
+    Note also that the examples in
+    this file use a threaded style.
+    For a different approach, see
+    the examples in the multi.pyn
+    example file.
     '''
 
     import threading
