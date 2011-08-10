db2df4a0ae8436e4bdc435484dfd47bd3d6f317c
Lee Harr <missive@hotmail.com>
2011-08-09 21:04:13 -0400

    fix up the descartes example

--- doc/examples_src/descartes_pynd/00000.py
+++ doc/examples_src/descartes_pynd/00000.py
@@ -1,16 +1,26 @@
 def show_all():
     import time
+
+    home()
+    notrack()
+
     xmin, ymin, xmax, ymax = viewcoords()
+
     funcs = globals().keys()
     funcs.sort()
     for fname in funcs:
         if fname.startswith('eq'):
-            clear()
             f = globals().get(fname)
+
+            clear()
+            home()
+
             grid()
             axes()
+
             color('rdark')
             plot(f)
             label(f)
             goto(xmax-50, ymin+50)
+
             time.sleep(5)
