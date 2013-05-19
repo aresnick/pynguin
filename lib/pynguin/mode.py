25e1ac6f2600e711aa8bce5647771e9563d9565d
Lee Harr <missive@hotmail.com>
2013-05-19 14:18:29 -0400

    add pie option to arc

--- lib/pynguin/mode.py
+++ lib/pynguin/mode.py
@@ -253,8 +253,8 @@ class ModeBase(Pynguin):
     def circle(self, r, center=False):
         self._pyn.circle(r, center)
 
-    def arc(self, r, extent, center=False, move=True):
-        self._pyn.arc(r, extent, center, move)
+    def arc(self, r, extent, center=False, move=True, pie=False):
+        self._pyn.arc(r, extent, center, move, pie)
 
     def square(self, side, center=False):
         self._pyn.square(side, center)
